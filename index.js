3f47540dcea737f1bdaf91db23010d3f7570301e
unknown <christian@stikman.com>
2021-05-16 08:49:52 -0700

    Add previous used code prompts to adjust, as well as the base readme template, and the js that creates the file

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const generateReadMe = (answers) =>
+  `# ReadMe-Generator
+  ReadMe File Generator using Node
+  
+  # Description
+  
+  # Images
+  
+  # Installation
+  
+  # Live Project
+  
+  # Credit
+  
+  # License`;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  .then((answers) => {
+    const readMePageContent = generateReadMe(answers);
+    fs.writeFile('README.md', readMePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
